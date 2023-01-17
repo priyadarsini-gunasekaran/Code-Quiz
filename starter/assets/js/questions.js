304e90dbd5f4c22785270f5bdf0acc61df1dc47d
tm28823289 <77493239+tm28823289@users.noreply.github.com>
2023-01-17 20:05:41 +0000

    functionally working code

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,53 +1,53 @@
   //Define questions (Object)
   var questionSource = [
     {
-        question: "Questions 1 : Inside which HTML element do we put the JavaScript?",
+        title: "Questions 1 : Inside which HTML element do we put the JavaScript?",
         choices: ["a. <javascript>", "b. <script>", "c. <js>", "d. <scripting>"],
-        answer: "b"
+        answer: "b. <script>"
     },
     {
-        question: "Questions 2 : How to write an IF statement in JavaScript?",
+        title: "Questions 2 : How to write an IF statement in JavaScript?",
         choices: ["a. if i=5", "b. if (i==5)", "c. if i=5 then", "d. if i==5 then"],
-        answer: "c"
+        answer: "b. if (i==5)"
     },
     {
-        question: "Questions 3 : How do you create a function in JavaScript",
+        title: "Questions 3 : How do you create a function in JavaScript",
         choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
-        answer: "b"
+        answer: "b. function myFunction()"
     },
     {
-        question: "Questions 4 : How do you call a function named myFunction?",
+        title: "Questions 4 : How do you call a function named myFunction?",
         choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
-        answer: "c"
+        answer: "c. myFunction()"
     },
     {
-        question: "Questions 5 : To see if two variables are equal in an if / else statement you would use ____.",
+        title: "Questions 5 : To see if two variables are equal in an if / else statement you would use ____.",
         choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
-        answer: "b"
+        answer: "b. =="
     },
     {
-        question: "Questions 6 : The first index of an array is ____.",
+        title: "Questions 6 : The first index of an array is ____.",
         choices: ["a. 0", "b. 1", "c. 8", "d. any"],
-        answer: "a"
+        answer: "a. 0"
     },
     {
-        question: "Questions 7 : How does a WHILE loop start?",
+        title: "Questions 7 : How does a WHILE loop start?",
         choices: ["a. while i = 1 to 10", "b. while (i<=10)", "c. while(i<=10;i++)", "d. while{i=6}"],
-        answer: "b"
+        answer: "b. while (i<=10)"
     },
     {
-        question: "Questions 8 : Which event occurs when the user clicks on an HTML elementHow does a FOR loop start?",
+        title: "Questions 8 : Which event occurs when the user clicks on an HTML elementHow does a FOR loop start?",
         choices: ["a. for (i=0;i<=5)", "b. for(i=0;i<=5;i++)", "c. for i = 1 to 5", "d. for (i<=5;i++)"],
-        answer: "b"
+        answer: "b. for(i=0;i<=5;i++)"
     },
     {
-        question: "Questions 9 : How do you round the number 7.25, to the nearest integer?",
+        title: "Questions 9 : How do you round the number 7.25, to the nearest integer?",
         choices: ["a. rnd(7.25)", "b. Math.round(7.25)", "c. Math.rnd(7.25)", "d. round(7.25)"],
-        answer: "b"
+        answer: "b. Math.round(7.25)"
     },
     {
-        question: "Questions 10 : How do you find the number with the highest value of x and y?",
+        title: "Questions 10 : How do you find the number with the highest value of x and y?",
         choices: ["a. ceil(x,y)", "b. Math.ceil(x,y)", "c. top(x,y)", "d. Math.max(x,y)"],
-        answer: "d"
+        answer: "d. Math.max(x,y)"
     }
   ];
